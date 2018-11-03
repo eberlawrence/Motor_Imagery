932aada349624846d6103ef78ee396c2bb7cd3ea
Eber Lawrence <eberlawrence@hotmail.com>
2018-11-03 15:49:23 -0300

    Updated.

--- Motor_Imagery_Protocol/Motor_Imagery_Protocol/Form1.Designer.cs
+++ Motor_Imagery_Protocol/Motor_Imagery_Protocol/Form1.Designer.cs
@@ -28,21 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lbStartMessage1 = new System.Windows.Forms.Label();
+            this.pnOne = new System.Windows.Forms.Panel();
+            this.BtAvancar = new System.Windows.Forms.Button();
+            this.lbStartMessage2 = new System.Windows.Forms.Label();
+            this.BtColeta = new System.Windows.Forms.Button();
+            this.pnTwo = new System.Windows.Forms.Panel();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.TimerOne = new System.Windows.Forms.Timer(this.components);
+            this.pbMain = new System.Windows.Forms.PictureBox();
+            this.pnOne.SuspendLayout();
+            this.pnTwo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lbStartMessage1
+            // 
+            this.lbStartMessage1.AutoSize = true;
+            this.lbStartMessage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStartMessage1.Location = new System.Drawing.Point(55, 74);
+            this.lbStartMessage1.Name = "lbStartMessage1";
+            this.lbStartMessage1.Size = new System.Drawing.Size(0, 46);
+            this.lbStartMessage1.TabIndex = 0;
+            // 
+            // pnOne
+            // 
+            this.pnOne.Controls.Add(this.BtAvancar);
+            this.pnOne.Controls.Add(this.lbStartMessage2);
+            this.pnOne.Controls.Add(this.lbStartMessage1);
+            this.pnOne.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnOne.Location = new System.Drawing.Point(0, 0);
+            this.pnOne.Name = "pnOne";
+            this.pnOne.Size = new System.Drawing.Size(335, 450);
+            this.pnOne.TabIndex = 1;
+            // 
+            // BtAvancar
+            // 
+            this.BtAvancar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtAvancar.Location = new System.Drawing.Point(12, 123);
+            this.BtAvancar.Name = "BtAvancar";
+            this.BtAvancar.Size = new System.Drawing.Size(108, 48);
+            this.BtAvancar.TabIndex = 2;
+            this.BtAvancar.Text = "Avançar";
+            this.BtAvancar.UseVisualStyleBackColor = true;
+            this.BtAvancar.Click += new System.EventHandler(this.BtInicio_Click);
+            // 
+            // lbStartMessage2
+            // 
+            this.lbStartMessage2.AutoSize = true;
+            this.lbStartMessage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStartMessage2.Location = new System.Drawing.Point(94, 91);
+            this.lbStartMessage2.Name = "lbStartMessage2";
+            this.lbStartMessage2.Size = new System.Drawing.Size(0, 46);
+            this.lbStartMessage2.TabIndex = 1;
+            // 
+            // BtColeta
+            // 
+            this.BtColeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtColeta.Location = new System.Drawing.Point(48, 57);
+            this.BtColeta.Name = "BtColeta";
+            this.BtColeta.Size = new System.Drawing.Size(167, 35);
+            this.BtColeta.TabIndex = 3;
+            this.BtColeta.Text = "Iniciar Coleta";
+            this.BtColeta.UseVisualStyleBackColor = true;
+            this.BtColeta.Click += new System.EventHandler(this.BtColeta_Click);
+            // 
+            // pnTwo
+            // 
+            this.pnTwo.Controls.Add(this.pbMain);
+            this.pnTwo.Controls.Add(this.tbName);
+            this.pnTwo.Controls.Add(this.BtColeta);
+            this.pnTwo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnTwo.Location = new System.Drawing.Point(485, 0);
+            this.pnTwo.Name = "pnTwo";
+            this.pnTwo.Size = new System.Drawing.Size(315, 450);
+            this.pnTwo.TabIndex = 4;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(48, 100);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(167, 20);
+            this.tbName.TabIndex = 4;
+            this.tbName.Text = "Voluntário";
+            // 
+            // TimerOne
+            // 
+            this.TimerOne.Interval = 1;
+            this.TimerOne.Tick += new System.EventHandler(this.TimerOne_Tick);
+            // 
+            // pbMain
+            // 
+            this.pbMain.Location = new System.Drawing.Point(48, 126);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(372, 507);
+            this.pbMain.TabIndex = 5;
+            this.pbMain.TabStop = false;
             // 
             // FormMI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnTwo);
+            this.Controls.Add(this.pnOne);
             this.Name = "FormMI";
             this.Text = "Motor Imagery Protocol";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormMI_Load);
+            this.pnOne.ResumeLayout(false);
+            this.pnOne.PerformLayout();
+            this.pnTwo.ResumeLayout(false);
+            this.pnTwo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMain)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbStartMessage1;
+        private System.Windows.Forms.Panel pnOne;
+        private System.Windows.Forms.Label lbStartMessage2;
+        private System.Windows.Forms.Button BtAvancar;
+        private System.Windows.Forms.Button BtColeta;
+        private System.Windows.Forms.Panel pnTwo;
+        private System.Windows.Forms.Timer TimerOne;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.PictureBox pbMain;
     }
 }
 
--- Motor_Imagery_Protocol/Motor_Imagery_Protocol/Form1.cs
+++ Motor_Imagery_Protocol/Motor_Imagery_Protocol/Form1.cs
@@ -7,19 +7,162 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Motor_Imagery_Protocol
 {
     public partial class FormMI : System.Windows.Forms.Form
     {
+        // Ajustar a posicição de panels, labels, button...
+        int x; int y;
+
+        // Variavel para embaralhar as classes
+        Random randonNum = new Random();
+
+        // Classes que serão embaralhadas
+        List<int> TwoMoviments = new List<int>() { 1, 2};
+
+        //Dicionário para colocar as listas com a ordem de imagens e seu indice de 0 a 39
+        Dictionary<int, List<int>> data = new Dictionary<int, List<int>>();
+
+        // Lista de imagens dos movimentos que devem ser executados
+        List<Bitmap> imageList = new List<Bitmap>();
+
+        //Carregando todas as imagens usadas
+        Bitmap img0 = new Bitmap(@"C:\Users\BioLab\Desktop\GitHub\Motor_Imagery\Motor_Imagery_Protocol\img\nada.jpg");
+        Bitmap img1 = new Bitmap(@"C:\Users\BioLab\Desktop\GitHub\Motor_Imagery\Motor_Imagery_Protocol\img\NeutroE.jpg");
+        Bitmap img2 = new Bitmap(@"C:\Users\BioLab\Desktop\GitHub\Motor_Imagery\Motor_Imagery_Protocol\img\NeutroD.jpg");
+        Bitmap img3 = new Bitmap(@"C:\Users\BioLab\Desktop\GitHub\Motor_Imagery\Motor_Imagery_Protocol\img\FechaE.jpg");
+        Bitmap img4 = new Bitmap(@"C:\Users\BioLab\Desktop\GitHub\Motor_Imagery\Motor_Imagery_Protocol\img\FechaD.jpg");
+
+        //Salvar o numero da coleta
+        int a = 1;
+        bool iRest = true;
+        //Contagem da quantidade do numero de movimentos, total e parcial. contiInitial = incrementa as informações iniciais
+        int countInitial = 0; int countT = 0; int countP = 0;
+        // incrementam a lista de imagens e a imagem dentro da lista
+        int countTwo = 0; int countOne = 0;
+
+        private void ImageShow()
+        {
+            imageList.Add(img0);
+            imageList.Add(img1);
+            imageList.Add(img2);
+            imageList.Add(img3);
+            imageList.Add(img4);
+
+            for (int i = 0; i < 10; i++)
+            {
+                data.Add(i, TwoMoviments.OrderBy(s => randonNum.Next()).Take(2).ToList());
+            }
+        }
+        
         public FormMI()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FormMI_Load(object sender, EventArgs e)
         {
+            pnOne.Dock = DockStyle.Fill;
+            pnTwo.Dock = DockStyle.Fill;
+            pnTwo.Visible = false;
 
+
+
+            x = (pnOne.Size.Width - BtAvancar.Width) / 2;
+            y = (pnOne.Size.Height)*100 / 120 ;
+            BtAvancar.Location = new Point(x, y);
+
+            lbStartMessage1.Text = "Protocolo de Coleta";
+            x = (pnOne.Size.Width - lbStartMessage1.Width) / 2;
+            y = (pnOne.Size.Height - lbStartMessage1.Height - 50) / 2;
+            lbStartMessage1.Location = new Point(x, y);
+
+            lbStartMessage2.Text = "Imagética Motora";
+            x = (pnOne.Size.Width - lbStartMessage2.Width) / 2;
+            y = (pnOne.Size.Height - lbStartMessage2.Height + 50) / 2;
+            lbStartMessage2.Location = new Point(x, y);
+
+        }
+
+        private void BtInicio_Click(object sender, EventArgs e)
+        {
+            pnOne.Visible = false;
+            pnTwo.Visible = true;
+
+            x = (pnTwo.Size.Width - pbMain.Width) / 2;
+            y = (pnTwo.Size.Height - pbMain.Height) / 2;
+            pbMain.Location = new Point(x, y);
+        }
+
+        private void BtColeta_Click(object sender, EventArgs e)
+        {
+            for (int i = data.Count; i >= 0; i--)
+            {
+                data.Remove(i);
+            }
+            if (BtColeta.Text == "Iniciar Coleta")
+            {
+                BtColeta.Text = "Parar Coleta";
+                ImageShow();
+                TimerOne.Start();
+                DataSave();
+            }
+            else if (BtColeta.Text == "Parar Coleta")
+            {
+                BtColeta.Text = "Iniciar Coleta";
+                TimerOne.Stop();
+            }
+        }
+
+        private void TimerOne_Tick(object sender, EventArgs e)
+        {
+            if (iRest == true)
+            {
+                TimerOne.Interval = 2000;
+                pbMain.Image = imageList[0];
+                iRest = false;
+                switch (countTwo)
+                {
+                    case 10:
+                        countTwo = 0;
+                        TimerOne.Stop();
+                        break;
+                }
+            }
+            else
+            {
+                Console.Beep(500, 1000);
+
+                TimerOne.Interval = 4000;
+                pbMain.Image = imageList[data[countTwo][countOne++]];
+                iRest = true;
+                switch (countOne)
+                {
+                    case 2:
+                        countOne = 0;
+                        countTwo++;
+                        break;
+
+                }
+            }
+        }
+
+
+
+        private void DataSave()
+        {
+            var File = @"C:\Users\BioLab\Desktop\GitHub\Motor_Imagery\Motor_Imagery_Protocol\Respostas\" + tbName.Text + "-Resposta.txt";
+            using (var write = new StreamWriter(File))
+                foreach (KeyValuePair<int, List<int>> N in data)
+                {
+                    write.Write("[{0}]\r\n", N.Key);
+                    foreach (var item in N.Value)
+                    {
+                        write.Write("{0}\r\n", item);
+                    }
+                }
         }
     }
 }
